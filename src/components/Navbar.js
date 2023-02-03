8953a70c032bb1786f64103c5217b384c2545316
Jyotiprakash Sahoo <jpsahoo3@gmail.com>
2023-02-03 11:25:15 +0530

    Note component Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    let location = useLocation();
+        console.log(location.pathname )
+    React.useEffect(() => {
+
+    }, [location]);
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">Navbar</Link>
@@ -11,14 +16,14 @@ const Navbar = () => {
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                    <li className="nav-item">
+                        <Link className={`nav-link ${location.pathname === "/" ? "active" :""}`} to="/">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className={`nav-link ${location.pathname === "/about" ? "active" :""}`} to="/about">About</Link>
                     </li>
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
+                <form className="form-inline my-2 my-lg-0"> 
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,68 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name" : "JP",
-        "class" : "5b"
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "Larry",
-                "class" : "10b"
-            })
-        }, 2000);
-    }
+
+    const notesInitial = [
+        {
+          "_id": "63d9ed20890eb04abc7e7721",
+          "user": "63d7a62d199032ebc078068c",
+          "title": "My Title1",
+          "description": "Please wake up early1",
+          "tag": "Personal1",
+          "date": "2023-02-01T04:40:00.610Z",
+          "__v": 0
+        },
+        {
+          "_id": "63db5ddc1bf7f0e2a5ca2ee1",
+          "user": "63d7a62d199032ebc078068c",
+          "title": "My Title1",
+          "description": "Please wake up early1",
+          "tag": "Personal1",
+          "date": "2023-02-02T06:53:16.593Z",
+          "__v": 0
+        },
+        {
+          "_id": "63db5df31bf7f0e2a5ca2ee4",
+          "user": "63d7a62d199032ebc078068c",
+          "title": "My Title",
+          "description": "Please wake up early",
+          "tag": "Personal",
+          "date": "2023-02-02T06:53:39.518Z",
+          "__v": 0
+        },
+        {
+            "_id": "63d9ed20890eb04abc7e7721",
+            "user": "63d7a62d199032ebc078068c",
+            "title": "My Title1",
+            "description": "Please wake up early1",
+            "tag": "Personal1",
+            "date": "2023-02-01T04:40:00.610Z",
+            "__v": 0
+          },
+          {
+            "_id": "63db5ddc1bf7f0e2a5ca2ee1",
+            "user": "63d7a62d199032ebc078068c",
+            "title": "My Title1",
+            "description": "Please wake up early1",
+            "tag": "Personal1",
+            "date": "2023-02-02T06:53:16.593Z",
+            "__v": 0
+          },
+          {
+            "_id": "63db5df31bf7f0e2a5ca2ee4",
+            "user": "63d7a62d199032ebc078068c",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "Personal",
+            "date": "2023-02-02T06:53:39.518Z",
+            "__v": 0
+          }
+      ]
+      
+      const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
